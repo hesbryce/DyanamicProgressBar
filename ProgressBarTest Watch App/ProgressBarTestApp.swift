a2519bf2b72e3ebf624243dca18eb14f0b75735b
Bryce Ellis <zbellis03@gmail.com>
2023-04-14 11:05:09 -0500

    Initial Commit

---
+++ ProgressBarTest Watch App/ProgressBarTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressBarTestApp.swift
+//  ProgressBarTest Watch App
+//
+//  Created by Bryce Ellis on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ProgressBarTest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProgressBarTest Watch AppUITests/ProgressBarTest_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ProgressBarTest_Watch_AppUITestsLaunchTests.swift
+//  ProgressBarTest Watch AppUITests
+//
+//  Created by Bryce Ellis on 4/14/23.
+//
+
+import XCTest
+
+final class ProgressBarTest_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
