776d43267e1a827e6710bb9fda447c9c005e612c
Bryce Ellis <zbellis03@gmail.com>
2023-04-14 13:51:55 -0500

    Example of a Dynamic Progress Bar for Stamina Bar

--- ProgressBarTest Watch App/ContentView.swift
+++ ProgressBarTest Watch App/ContentView.swift
@@ -1,21 +1,35 @@
-//
-//  ContentView.swift
-//  ProgressBarTest Watch App
-//
-//  Created by Bryce Ellis on 4/14/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    var progress: CGFloat = 0.25// Bind to your user's heart rate value
+    
+    let outerBarWidth: CGFloat = 200 // Width of the outer bar
+    let barHeight: CGFloat = 10 // Height of both bars
+    let cornerRadius: CGFloat = 5 // Rounded corner radius
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack(alignment: .leading) {
+                Rectangle() // Gray background bar
+                    .foregroundColor(.gray)
+                    .frame(width: outerBarWidth, height: barHeight)
+                    .cornerRadius(cornerRadius)
+                
+                // Colored progress bar
+                Rectangle()
+                    .foregroundColor(.blue) // Change this to your desired color
+                    .frame(width: outerBarWidth * progress, height: barHeight)
+                    .cornerRadius(cornerRadius)
+                    .overlay(
+                        // Label showing progress percentage
+                        Text("\(Int(progress * 100))%")
+                            .foregroundColor(.white)
+                            .font(.system(size: 10))
+                            .position(x: outerBarWidth * progress / 2 - 10, y: barHeight / 2)
+
+                    )
+            }
         }
-        .padding()
     }
 }
 
@@ -24,3 +38,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+ 
